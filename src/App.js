fdc2302381899638ecc0d511b73ebc2be20e648e
amira-rhouma <rhouma.amira95@gmail.com>
2021-04-30 08:53:06 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './Component/Form'
+
+function App() {
+  return (
+    
+
+<div class="form-group">
+    <Form/>
+    
+  </div>
+    
+  );
+}
+
+export default App;
